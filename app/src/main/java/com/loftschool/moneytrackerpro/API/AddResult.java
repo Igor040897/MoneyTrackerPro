1e3d8372e16a232197da8cd357b0ed6176759fcd
Igor040897 <ihor.kolisnyk@nure.ua>
2017-08-31 00:07:07 +0300

    fix bug

--- app/src/main/java/com/loftschool/moneytrackerpro/API/AddResult.java
+++
@@ -1,9 +0,0 @@
-package com.loftschool.moneytrackerpro.API;
-
-/**
- * Created by fanre on 7/1/2017.
- */
-
-public class AddResult extends Result {
-    public int id;
-}
--- app/src/main/java/com/loftschool/moneytrackerpro/API/AuthResult.java
+++
@@ -1,9 +0,0 @@
-package com.loftschool.moneytrackerpro.API;
-
-/**
- * Created by fanre on 7/6/2017.
- */
-
-public class AuthResult extends Result {
-    public String authToken;
-}
